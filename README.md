dddaff3c0ee03b4a7bef05cc8dacff8be332f052
Miki Yutani <mkyutani@gmail.com>
2022-12-18 16:57:38 +0900

    Support excel embeded pdf table and geocode cache

--- README.md
+++ README.md
@@ -1,1 +1,45 @@
-# bunkyo-iryo-kyujitsutoban
+# 文京区「今月の当番医」をStarSeekerに描画する
+
+## 作業ディレクトリを作成
+
+作業ディレクトリ(例: work)を作成する。
+
+```
+~/bunkyo-iryo-kyujitsutoban$ mkdir work
+~/bunkyo-iryo-kyujitsutoban$ cd work
+```
+## PDFからデータを抽出
+
+Excelのデータ取得機能でpdfからデータを抽出する。
+
+* 文京区の[「今月の当番医」ページ](https://www.city.bunkyo.lg.jp/hoken/iryo/kyujithutouban/5gatsubun.html)からpdfをダウンロード
+* Excelを開き、「データ」→「データの取得」→「ファイルから」→「PDFから」で該当するPDFを選択
+* ナビゲーターで「Table<番号>」のうちデータの入っているテーブルを選択し「データ変換」を押す
+* Power Queryエディターが開くので、メニューから「閉じて読み込む」を実行しexcelに読み込む
+* 日付(A列)の行がほぼズレているので、PDFと比較しつつ、同じ日付となる最初の行に手で移動する。(元PDFがExcelセル結合をしていることによる限界)
+* Excelを保存し、作業ディレクトリに移動
+
+## データCSVを作成
+
+[xlsx2csv](https://github.com/dilshod/xlsx2csv)でxlsxをcsvに変換する。
+
+```
+~/bunkyo-iryo-kyujitsutoban/work$ xlsx2csv -s 1 toban-ikashika2301.xlsx toban-ikashika2301.csv
+```
+
+## StarSeekerデータを作成
+
+`convert.py`を使って[StarSeeker](https://github.com/c-3lab/StarSeeker)のoperatorで登録するデータを作成する。
+なお、作業ディレクトリにできる`.geocode.cache`ファイルは同じ住所で何度もジオコーディングAPIを呼び出さないためのキャッシュであり、残しておくと次に実行するときの処理速度が速くなる。
+
+```
+~/bunkyo-iryo-kyujitsutoban/work$ ../convert.py --dir work --month 2301 toban-ikashika2301.csv
+```
+
+`point.csv`と`point_data.csv`が生成されているので適宜名前を変更する。
+
+```
+~/bunkyo-iryo-kyujitsutoban/work$ mv point.csv point.toban-ikashika2301.csv
+~/bunkyo-iryo-kyujitsutoban/work$ mv point_data.csv point_data.toban-ikashika2301.csv
+```
+## 
--- convert.py
+++ convert.py
@@ -3,6 +3,7 @@
 import csv
 import datetime
 import errno
+import json
 import os
 import re
 import requests
@@ -56,10 +57,39 @@ def get(address):
 
     return res.text
 
-def geocode(address):
+class GeocodeCache:
+
+    def __init__(self, cache_file_dir, cache_file_name='.geocode.cache', encoding='utf-8'):
+        self.cache = {}
+        self.cache_file_path = os.path.join(cache_file_dir, cache_file_name)
+        self.encoding = encoding
+
+    def load(self):
+        if os.path.exists(self.cache_file_path):
+            with open(self.cache_file_path, 'r', encoding=self.encoding) as fdcache:
+                self.cache = json.load(fdcache)
+            print(f'loaded geocode cache (N={len(self.cache)}) from {self.cache_file_path}')
+
+    def save(self):
+        with open(self.cache_file_path, 'w', encoding=self.encoding) as fdcache:
+            json.dump(self.cache, fdcache)
+        print(f'saved geocode cache (N={len(self.cache)}) in {self.cache_file_path}')
+
+    def get(self, location):
+        return self.cache.get(location)
+
+    def set(self, location, value):
+        self.cache[location] = value
+
+def geocode(address, geocode_cache):
+
+    if type(geocode_cache) is GeocodeCache:
+        geocode = geocode_cache.get(address)
+        if geocode is not None:
+            return (geocode['lat'], geocode['lng'])
 
     wait = 12
-    print(f'geocodes {address} after {wait} seconds ... ', end='', file=sys.stderr)
+    print(f'geocodes {address} after {wait} seconds ... ', end='', flush=True, file=sys.stderr)
     time.sleep(wait)
 
     text = get(address)
@@ -72,9 +102,9 @@ def geocode(address):
         error = str(root.find('error').text)
         print(f'error by remote api ({error})', file=sys.stderr)
 
-        for i in range(0, 10):
+        for i in range(1, 10):
             wait = wait + 10 * i
-            print(f'geocodes again {address} after {wait} seconds ... ', end='', file=sys.stderr)
+            print(f'geocodes again {address} after {wait} seconds ... ', end='', flush=True, file=sys.stderr)
             time.sleep(wait)
 
             text = get(address)
@@ -95,44 +125,76 @@ def geocode(address):
     lng = coordinate.find('lng').text
 
     print(f'{lat}, {lng}', file=sys.stderr)
+
+    if type(geocode_cache) is GeocodeCache:
+        geocode_cache.set(address, {
+            'lat': lat,
+            'lng': lng
+        })
+
     return (lat, lng)
 
-def create_locations(data):
+def get_year_and_month(month_expr):
 
-    locations = {}
-    day = 0
-
-    month_matched = re.match(r'^\d+', data[0][0])
-    if month_matched is None:
-        return errno.EIO
-    month = int(month_matched.group())
+    year = 0
+    month = 0
 
     now_dt = datetime.datetime.now()
+    if month_expr is None:
+        month = now_dt.month
+    elif re.match(r'^\d+$', month_expr):
+        if len(month_expr) == 6:
+            year = int(month_expr[:4])
+            month = int(month_expr[4:])
+        elif len(month_expr) == 4:
+            year = int(month_expr[:2]) + (now_dt.year % 100)
+            month = int(month_expr[2:])
+        elif len(month_expr) > 0 and len(month_expr) < 3:
+            month = int(month_expr)
+        else:
+            return None
+    else:
+        return None
+
     if int(month) - now_dt.month >= -3:
         year = now_dt.year
     else:
         year = now_dt.year + 1
 
-    locations_at_date = {}
+    return [ year, month ]
+
+def create_locations(data, month_expr, geocode_cache):
+
+    month_vec = get_year_and_month(month_expr)
+    if month_vec is None:
+        return errno.EPERM
+
+    year = month_vec[0]
+    month = month_vec[1]
+    day = 0
+
+    locations = {}
+
     for original_row in data:
         inserting = True
-        tag_line = False
+        new_day = False
 
-        if len(original_row) < 12:
+        if len(original_row) < 16:
             inserting = False
         else:
-            if re.fullmatch(r'\([日月火水木金土]\)', original_row[0]):
-                tag_line = True
-            elif re.fullmatch(r'\d+', original_row[0]):
-                day = int(original_row[0])
-            elif len(original_row[0]) == 0:
-                pass
-            else:
+            date_expr = original_row[0]
+            full_date_pattern = re.fullmatch(r'^(\d+)(\([日月火水木金土]\))$', date_expr)
+            if full_date_pattern:
+                day = int(full_date_pattern.group(1))
+                new_day = True
+            elif len(date_expr) > 0:
                 inserting = False
 
-            name = original_row[1]
-            dept = original_row[2]
-            attr = original_row[3] + original_row[4]
+        if inserting is True:
+
+            name = re.sub('\s', '', ''.join(original_row[1:6]))
+            dept = original_row[6]
+            attr = re.sub('\s', '', original_row[7] + original_row[8])
             if attr == '○○':
                 schedule = '昼間・準夜'
             elif attr == '○―':
@@ -141,35 +203,35 @@ def create_locations(data):
                 schedule = '準夜'
             elif attr == '――':
                 schedule = '不明'
-            address = re.sub('\s', '', ''.join(original_row[5:11]))
-            tel = original_row[11]
+            address = re.sub('\s', '', ''.join(original_row[9:14]))
+            tel = re.sub('\s', '', ''.join(original_row[14:]))
 
-        if inserting is True:
-            (lat, lng) = geocode(address)
+            (lat, lng) = geocode(address, geocode_cache)
 
-            locations_at_date.update({
-                f'{name}【{dept}】': {
-                    'schedule': schedule,
-                    'address': address,
-                    'tel': tel,
-                    'lat': lat,
-                    'lng': lng
+            date_key = yymmdd(year, month, day)
+            if date_key in locations:
+                locations[yymmdd(year, month, day)].update({
+                    f'{name}【{dept}】': {
+                        'schedule': schedule,
+                        'address': address,
+                        'tel': tel,
+                        'lat': lat,
+                        'lng': lng
+                    }
+                })
+            else:
+                locations[date_key] = {
+                    f'{name}【{dept}】': {
+                        'schedule': schedule,
+                        'address': address,
+                        'tel': tel,
+                        'lat': lat,
+                        'lng': lng
+                    }
                 }
-            })
         else:
             line = re.sub('\s', '', ','.join(original_row))
-            print(f'Skipped [{line}]', file=sys.stderr)
-
-        if tag_line is True:
-            locations.update({
-                yymmdd(year, month, day): locations_at_date
-            })
-            locations_at_date = {}
-
-    if len(locations_at_date) > 0:
-        print('Some locations without schedule', file=sys.stderr)
-        print(locations_at_date)
-        return errno.EIO
+            print(f'skipped [{line}]', file=sys.stderr)
 
     return locations
 
@@ -186,9 +248,14 @@ def output(locations, dir):
 
     with open(point_path, 'w') as fdp:
         with open(point_data_path, 'w') as fdpd:
+            color_palette = [
+                '#7f4500', '#008000', '#000080', '#b22222',
+                '#ffdab9', '#00ff00', '#8a2be2', '#00ffff'
+            ]
+            color_index = 0
             for location_at_date in sorted(locations):
                 entity_type = 'Date' + re.sub('-', '', location_at_date)
-                print(f'{location_at_date},{tenant},{servicepath},{category},{entity_type},#7f0000,〇,location,time,名称,organization,0,1,住所,address,0,2,電話番号,tel,0,3,時間帯,schedule,0,4,,,,', file=fdp)
+                print(f'{location_at_date},{tenant},{servicepath},{category},{entity_type},{color_palette[color_index % 8]},〇,location,time,名称,organization,0,1,住所,address,0,2,電話番号,tel,0,3,時間帯,schedule,0,4,,,,', file=fdp)
                 serial = 1
                 for org in locations[location_at_date]:
                     location = locations[location_at_date][org]
@@ -204,6 +271,7 @@ def output(locations, dir):
                     print(f'{entity_type}{serial:03},{tenant},{servicepath},{entity_type},location,geo:point,"{lat}, {lng}"', file=fdpd)
                     print(f'{entity_type}{serial:03},{tenant},{servicepath},{entity_type},time,DateTime,{now_string}', file=fdpd)
                     serial = serial + 1
+                color_index = color_index + 1
 
 def main():
 
@@ -217,7 +285,8 @@ def main():
 
     parser = argparse.ArgumentParser(description='make kyujitu iryo list of bunkyo city', formatter_class=SortingHelpFormatter)
     parser.add_argument('meddent', nargs=1, metavar='CSV', help='medical and dental schedule csv')
-    parser.add_argument('-d', '--dir', nargs=1, metavar='DIR', default=['.'], help='working directory')
+    parser.add_argument('--month', nargs=1, default=[None], help='year and month (ex. 202301, 2301 or 1)')
+    parser.add_argument('--dir', nargs=1, metavar='DIR', default=['.'], help='working directory')
     parser.add_argument('--encoding', nargs=1, metavar='ENCODING', default=['utf-8'], help='encoding of source csv file')
 
     if len(sys.argv) < 2:
@@ -226,11 +295,16 @@ def main():
 
     args = parser.parse_args()
 
-    locations = create_locations(csv_to_matrix(args.meddent[0], args.dir[0], args.encoding[0]))
+    geocode_cache = GeocodeCache(args.dir[0])
+    geocode_cache.load()
+
+    locations = create_locations(csv_to_matrix(args.meddent[0], args.dir[0], args.encoding[0]), args.month[0], geocode_cache)
     if type(locations) is int:
         return locations
+    else:
+        output(locations, args.dir[0])
 
-    output(locations, args.dir[0])
+    geocode_cache.save()
 
     return 0
 
